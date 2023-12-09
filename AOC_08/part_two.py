a21de576c8d72006b0854f8097d86f9af1d41357
Shenya De Silva <shenya44@outlook.com>
2023-12-09 18:53:48 +0100

    Initial commit part_two.py

---
+++ AOC_08/part_two.py
@@ -0,0 +1,30 @@
+with open('input.txt', 'r') as input_data:
+    rows = [line.strip() for line in input_data.readlines()]
+lines = []
+for row in rows:
+	row = row.split(' ')
+	row = [int(x) for x in row]
+	lines.append(row)
+
+def get_diff(num, line, count):
+	diff = []
+	i = 0
+	while (i < num - 1):
+		diff.append(line[i] - line[i + 1])
+		i += 1
+	x = any(diff)
+	if (not x):
+		count += diff[0]
+		return(count)
+	if (x):
+		count += diff[0]
+		return(get_diff(num-1, diff, count))
+
+total = 0
+for line in lines:
+	val = get_diff(21, line, 0)
+	print(val)
+	final_val = line[0] + val
+	total += final_val
+
+print("Total", total)
