34be1f9cd96a7ab0d161a3be31afc375536c48bd
Shenya De Silva <shenya44@outlook.com>
2024-10-09 20:32:29 +0200

    Finally the recursion

--- exam/L04/Perfect_sum_problem/psp.c
+++ exam/L04/Perfect_sum_problem/psp.c
@@ -5,7 +5,7 @@ Given an array of integers and a sum,
 the task is to print all subsets of the 
 given array with a sum equal to a given sum.
 */
-#include <stdio.h>
+// #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -49,18 +49,6 @@ int ft_atoi(char *str)
 	return (n * sign);
 }
 
-// int *get_array(char **arr, int size)
-// {
-// 	int *int_arr = malloc(sizeof(int) * size);
-// 	int i = 0;
-// 	while (i < size)
-// 	{
-// 		int_arr[i] = ft_atoi(arr[i]);
-// 		i++;
-// 	}
-// 	return (int_arr);
-// }
-
 t_sum *get_array(char **arr, int size)
 {
 	t_sum *int_arr = malloc(sizeof(t_sum) * size);
@@ -75,15 +63,18 @@ t_sum *get_array(char **arr, int size)
 	return (int_arr);
 }
 
-// void	print_arr(int *arr, int size)
-// {
-// 	int i = 0;
-// 	while (i < size)
-// 	{
-// 		printf("%d ", arr[i]);
-// 		i++;
-// 	}
-// }
+int get_sum(t_sum *arr, int size)
+{
+	int i = 0;
+	int sum = 0;
+	while (i < size)
+	{
+		if (arr[i].check == 1)
+			sum += arr[i].val;
+		i++;
+	}
+	return (sum);
+}
 
 void	print_arr(t_sum *arr, int size)
 {
@@ -98,25 +89,75 @@ void	print_arr(t_sum *arr, int size)
 	write (1, "\n", 1);
 }
 
-// convert sum to int
-// convert char **arr to int **arr
-// write perfect sum algo
-// print combination
-	// convert int to char *str 
-	// print strs
+void	print_path(t_sum *arr, int size)
+{
+	int i = 0;
+	int count = 0;
+	while (i < size)
+	{
+		if (arr[i].check == 1)
+		{
+			write (1, arr[i].str, ft_strlen(arr[i].str));
+			write (1, " ", 1);
+			count++;
+		}
+		i++;
+	}
+	if (count)
+		write (1, "\n", 1);
+	// else
+	// 	write (1, "Nothing\n", 8);
+}
+
+t_sum *make_copy(t_sum *arr, int size)
+{
+	t_sum *copy = malloc(sizeof(t_sum) * size);
+	int i = 0;
+	while (i < size)
+	{
+		copy[i].val = arr[i].val;
+		copy[i].check = arr[i].check;
+		copy[i].str = arr[i].str;
+		i++;
+	}
+	return (copy);
+}
+
+void find_and_print(t_sum *arr, int size, int loc, int target)
+{
+	if (get_sum(arr, size) >= target || loc >= size - 1)
+	{
+		if (get_sum(arr, size) == target)
+			print_path(arr, size);
+		free(arr);
+		return;
+	}
+	else
+	{
+		while (loc < size)
+		{
+			t_sum *copy = make_copy(arr, size);
+			copy[loc].check = 1;
+			if (loc < size)
+				find_and_print(copy, size, loc + 1, target);
+			loc++;
+		}
+	}
+}
+
+/*
+convert sum to int
+convert char **arr to int **arr
+write perfect sum algo
+print combination - recursion
+	print strs
+*/
 int main(int argc, char **argv)
 {
 	if (argc < 3)
 		return (1);
-	// char *sum = argv[1];
-	// char **arr = argv + 1;
-
-	//int sum = ft_atoi(argv[1]);
-	// int *arr = get_array(argv + 1, argc - 1);
-	t_sum *arr = get_array(argv + 1, argc - 1);
-	print_arr(arr, argc - 1);
-
-	free(arr);
+	t_sum *arr = get_array(argv + 2, argc - 2);
+	find_and_print(arr, argc - 1, 0, ft_atoi(argv[1]));
 
 	return (0);
 }
