9cbd8037aa68bff783ebbc4c2b56ea03a00b41bf
Shenya De Silva <sde-silv@c3a4c3.42berlin.de>
2024-10-21 16:32:34 +0200

    Working...

--- exam/L04/pipeline/main.c
+++ exam/L04/pipeline/main.c
@@ -6,6 +6,12 @@
 #define STDIN 0
 #define STDOUT 1
 
+typedef struct s_cmd
+{
+	int in;
+	int out;
+}				t_cmd;
+
 void print_cmds(char **cmds[])
 {
 	for (int i = 0; cmds[i]; i++)
@@ -26,72 +32,91 @@ int count_cmds(char **cmds[])
 	return (i);
 }
 
-int exec_cmd(char *cmd[], int cmd_count, int fds[cmd_count][2], int loc)
+int exec_cmd(char *cmd[], int pipe_count, t_cmd arr[pipe_count], int i)
 {
-	(void)fds;
-	static int prev_fd = -1;
-	int fd[2];
-	if (pipe(fd) == -1)
-		return (1);
 	int pid = fork();
+	if (pid == -1)
+		return (1);
 	if (pid == 0)
 	{
-		if (prev_fd == -1)
+		if (arr[i].out != STDOUT)
 		{
-			dup2(fd[1], STDOUT);
-			close(fd[1]);
-			close(fd[0]);
+			if (dup2(arr[i].out, STDOUT) == -1)
+				return (1);
+			if (close(arr[i].out) == -1)
+				return (1);
 		}
-		if (loc == cmd_count - 1)
+		if (arr[i].in != STDIN)
 		{
-			dup2(prev_fd, STDIN);
-			close(prev_fd);
-			close(fd[0]);
+			if (dup2(arr[i].in, STDIN) == -1)
+				return (1);
+			if (close(arr[i].in) == 1)
+				return (1);
 		}
-		else
-		{
-			dup2(prev_fd, STDIN);
-			close(prev_fd);
-			dup2(fd[1], STDOUT);
-			close(fd[1]);
-			close(fd[0]);
-		}
-		execvp(cmd[0], cmd);
-		return (1);
+		if (execvp(cmd[0], cmd) == -1)
+			return (1);
 	}
 	else
 	{
-		prev_fd = fd[0];
-		if (prev_fd != -1)
-			close(fd[0]);
-		close(fd[1]);
-		return (0);
+		if (arr[i].out != STDOUT)
+			close(arr[i].out);
+		if (arr[i].in != STDIN)
+			close(arr[i].in);
 	}
+	return (0);
+}
+
+int setup_pipes(int cmd_count, t_cmd arr[cmd_count])
+{
+	for (int i = 0; i < cmd_count; i++)
+	{
+		arr[i].in = STDIN;
+		arr[i].out = STDOUT;
+	}
+	int pipe_count = cmd_count - 1;
+	if (pipe_count)
+	{
+		for (int i = 0; i < pipe_count; i++)
+		{
+			int fd[2];
+			if (pipe(fd) == -1)
+				return (0);
+			if (i == 0 || i + 1 == pipe_count)
+			{
+				if (i == 0)
+					arr[i].out = fd[1];
+				if (i + 1 == pipe_count)
+					arr[i+1].in = fd[0];
+			}
+			else
+			{
+				arr[i].in = fd[0];
+				arr[i].out = fd[1];
+			}
+		}
+	}
+	return (1);
 }
 
 int picoshell(char **cmds[])
 {
 	//print_cmds(cmds);
-	int status;
+	int status = 0;
 	int cmd_count = count_cmds(cmds);
-	int i = 0;
-	int fds[cmd_count][2];
-	while (i < cmd_count)
+	t_cmd arr[cmd_count];
+	if (!setup_pipes(cmd_count, arr))
+		return (1);
+	int pipe_count = cmd_count - 1;
+	int ret = 0;
+	int check = 0;
+	for (int i = 0; i < cmd_count; i++)
 	{
-		//first, last middle
-		if (i == 0 )
-			status = exec_cmd(cmds[i],cmd_count, fds, i);
-		else if (i == cmd_count - 1)
-			status = exec_cmd(cmds[i],cmd_count, fds, i);
-		else if (i > 0)
-			status = exec_cmd(cmds[i],cmd_count, fds, i);
-		i++;
+		ret = exec_cmd(cmds[i], pipe_count, arr, i);
+		if (ret && check == 0)
+			check = 1;
 	}
 	wait(&status);
-	// wait(NULL);
-	if (WIFEXITED(status))
-		WEXITSTATUS(status);
-	if (status)
+	if (status || check)
 		return (1);
 	else
 		return (0);
@@ -100,9 +125,9 @@ int picoshell(char **cmds[])
 int main(void)
 {
 	char *first[2] = {"ls",  NULL};
-	//char *second[3] = {"asjkfasf", "-la",  NULL};
+	//char *first[3] = {"asjkfasf", "-la",  NULL};
 	char *second[3] = {"grep", "Makefile" , NULL};
-	//char *first[2] = {"ls", NULL};
+	//char *second[2] = {"ls", NULL};
 
 	char **cmds[3] = {first, second, NULL};
 	//char **cmds[2] = {first, NULL};
