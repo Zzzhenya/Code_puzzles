ab2ab746b722a4c97ed48dca7bd6f08d5cbd6d9c
Shenya De Silva <shenya44@outlook.com>
2024-11-12 22:11:58 +0100

    Initial commit

---
+++ python/cybsec/script.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import shutil
+
+mainurl = 'https://toscrape.com/'
+ext = 'iso'
+
+# sends a get reuest to specified url
+# https://www.w3schools.com/python/ref_requests_get.asp
+# parameters url, params, allow_redirects, auth, cert, cookies, headers
+# proxies, stream, timeout, verify
+# returns: requests.response object, .text Returns the content of the response, in unicode
+page = requests.get(mainurl).text
+# https://realpython.com/beautiful-soup-web-scraper-python/
+# takes requests.response.text or .content as input - HTML content scraped
+# use appropriate parser and returns a beautifulsoup object
+soup = BeautifulSoup(page, 'html.parser')
+#https://brightdata.com/faqs/beautifulsoup/extract-image-urls
+# Step 5: Extract image URLs from HTML
+# Example: Find all image elements and extract their 'src' attributes
+image_elements = soup.find_all('img')
+image_urls = [img['src'] for img in image_elements]
+
+# Step 6: Print the extracted image URLs
+name = "img"
+num = 1
+for url in image_urls:
+	file_name = name + "_" + str(num) + re.split(r'/', url.lstrip('/'))[-1]
+	new_url = mainurl+url.removeprefix('./')
+	res = requests.get(new_url, stream = True)
+	if res.status_code == 200:
+		with open(file_name,'wb') as f:
+			shutil.copyfileobj(res.raw, f)
+		print('Image sucessfully Downloaded: ',file_name)
+	else:
+		print('Image Couldn\'t be retrieved')
+	num += 1
