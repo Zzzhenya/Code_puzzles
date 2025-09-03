b450af09922e0bf4389ee872c21dba8b77fcdc27
Shenya De Silva <sde-silv@c4c1c4.42berlin.de>
2025-09-03 17:36:41 +0200

    back to old logic

--- algo/bubble.py
+++ algo/bubble.py
@@ -20,52 +20,31 @@ end procedure
 '''
 import sys
 
-def sorted(arr, comp, size):
-	global counter
-	for i in range(1, size - 1):
-		comp+=1
-		counter+=1
-		if (arr[i - 1] > arr[i]):
-			return (False)
-	return (True)
-
 def bubble(arr, comp, size):
-	global counter
-	# for i in range(size):
-	# 	_sorted = True
-	# 	for j in range(size - i - 1):
-	# 	# for j in range(size - 1):
-	# 		comp += 1
-	# 		if arr[j] > arr[j+1]:
-	# 			arr[j], arr[j + 1] = arr[j + 1], arr[j]
-	# 			_sorted = False
-	# 	if _sorted:
-	# 		break
-	# return (arr, comp)
-	while sorted(arr, comp, size) == False:
-		swapped = False
-		for i in range(1, size - 1):
-			comp+=1
-			counter += 1
-			if (arr[i-1] > arr[i]):
-				arr[i], arr[i - 1] = arr[i - 1], arr[i]
-				swapped = True
-		# size = size - 1
+	for i in range(size):
+		_sorted = True
+		for j in range(size - i - 1):
+			# print(arr)
+		# for j in range(size - 1):
+			comp += 1
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+				_sorted = False
+		if _sorted:
+			break
 	return (arr, comp)
 
 def main():
-	global counter
 	comp = 0
 	arr = []
 	for i in sys.argv:
 		if i != sys.argv[0]:
 			arr.append(int(i))
-	# arr = [1, 2, 3 ,4, 7]
-	new_arr, comp = bubble(arr, comp, len(arr))
-	for x in new_arr:
-		print(x)
+	
+	arr, comp = bubble(arr, comp, len(arr))
+	
+	print(arr)
 	print("comp: " + str(comp))
-	print("counter: " + str(counter))
 
 if __name__ == "__main__":
 	main()
