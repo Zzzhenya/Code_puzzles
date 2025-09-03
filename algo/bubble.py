c5c34cde897bf627775bccd7d37733f585d471d3
Shenya De Silva <sde-silv@c4c1c4.42berlin.de>
2025-09-03 17:20:35 +0200

    sorted? logic with global variable

--- algo/bubble.py
+++ algo/bubble.py
@@ -1,6 +1,7 @@
 #ref: https://realpython.com/sorting-algorithms-python/#the-bubble-sort-algorithm-in-python
 #ref: https://en.wikipedia.org/wiki/Bubble_sort
 
+counter = 0
 '''
 procedure bubbleSort(A : list of sortable items)
     n := length(A)
@@ -19,11 +20,21 @@ end procedure
 '''
 import sys
 
-def bubble(arr, comp):
-	size = len(arr)
+def sorted(arr, comp, size):
+	global counter
+	for i in range(1, size - 1):
+		comp+=1
+		counter+=1
+		if (arr[i - 1] > arr[i]):
+			return (False)
+	return (True)
+
+def bubble(arr, comp, size):
+	global counter
 	# for i in range(size):
 	# 	_sorted = True
 	# 	for j in range(size - i - 1):
+	# 	# for j in range(size - 1):
 	# 		comp += 1
 	# 		if arr[j] > arr[j+1]:
 	# 			arr[j], arr[j + 1] = arr[j + 1], arr[j]
@@ -31,30 +42,30 @@ def bubble(arr, comp):
 	# 	if _sorted:
 	# 		break
 	# return (arr, comp)
-	for j in range(size):
+	while sorted(arr, comp, size) == False:
 		swapped = False
-		# for i in range(1, size - j):
 		for i in range(1, size - 1):
 			comp+=1
+			counter += 1
 			if (arr[i-1] > arr[i]):
 				arr[i], arr[i - 1] = arr[i - 1], arr[i]
 				swapped = True
-		size = size - 1
-		if swapped == False:
-			break
+		# size = size - 1
 	return (arr, comp)
 
 def main():
+	global counter
 	comp = 0
 	arr = []
 	for i in sys.argv:
 		if i != sys.argv[0]:
 			arr.append(int(i))
 	# arr = [1, 2, 3 ,4, 7]
-	new_arr, comp = bubble(arr, comp)
+	new_arr, comp = bubble(arr, comp, len(arr))
 	for x in new_arr:
 		print(x)
 	print("comp: " + str(comp))
+	print("counter: " + str(counter))
 
 if __name__ == "__main__":
 	main()
